d5d7c6143527f20f9fce47cbab2d06ac511a7437
Rafael Dantas <rafael.dantas.br@gmail.com>
2018-07-26 15:03:29 -0300

    Refactoring the logic

--- widget/src/_assets/_js/script.js
+++ widget/src/_assets/_js/script.js
@@ -1,20 +1,31 @@
 const list = document.getElementById('list');
 const request = new XMLHttpRequest();
-
-
 request.open('GET', 'http://trampos.co/api/oportunidades.json', true);
+
+// Gera um elemento <a> para ser inserido dentro do card
+const getLinkItem = (info) => {
+	const linkItem = document.createElement("a");
+	linkItem.href = info.opportunity.permalink;
+	linkItem.className = "link-item";
+
+	return linkItem;
+}
+
+// Gera um elemento <li> para ser inserido dentro do card
+const getListItem = (info) => {
+	const listItem = document.createElement("li");
+	listItem.className = "list-item";
+
+	return listItem;
+}
 
 request.onload = () => {
 	if(request.status >= 200 && request.status < 400) {
 		const data = JSON.parse(request.responseText);
 		const resp = data
 		.map((info) => {
-			const linkItem = document.createElement("a");
-			linkItem.href = info.opportunity.permalink;
-			linkItem.className = "link-item"
-			
-			const listItem = document.createElement("li");
-			listItem.className = "list-item";
+			const linkItem = getLinkItem(info);
+			const listItem = getListItem(info);
 
 			const textoInfo = document.createTextNode(info.opportunity.name);
 
